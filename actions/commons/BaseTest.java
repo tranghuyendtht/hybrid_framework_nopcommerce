c0ce70a46f536d7a64fb69d5a222bc93d88bd684
tranghuyendtht <trangdth202@gmail.com>
2023-05-12 15:01:10 +0700

    WebDriverManager_MultiBrowser

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,43 +1,42 @@
 package commons;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Parameters;
-
-import pageObjects.HomePageObject;
-
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseTest {
 	// Chứa các hàm dùng chung cho testcase
 	private WebDriver driver;
-	private String projectPath = System.getProperty("user.dir");
-	
-	
+
 	protected WebDriver getBrowserDriver(String browserName) {
-		System.out.println("Run on "+ browserName);
-		if(browserName.equals("firefox")) {
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-			driver = new FirefoxDriver();
-		} else if (browserName.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
-			driver = new ChromeDriver();
-		} else if (browserName.equals("edge")) {
-			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
-			driver = new EdgeDriver();
-		} else {
-			throw new RuntimeException("Browser name is invalid!");
+		browserName = browserName.toUpperCase();
+		System.out.println("Run on " + browserName);
+		switch(browserName) {
+		case "FIREFOX":
+			driver = WebDriverManager.firefoxdriver().create();
+			break;
+		case "CHROME":
+			driver = WebDriverManager.chromedriver().create();
+			break;
+		case "EDGE":
+			driver = WebDriverManager.edgedriver().create();
+			break;
+		case "COCCOC":
+			ChromeOptions options = new ChromeOptions();
+			options.setBinary("C:\\Program Files\\CocCoc\\Browser\\Application\\browser.exe");
+			driver = WebDriverManager.chromedriver().driverVersion("111.0.5563.64").capabilities(options).create();
+			break;
+		case "OPERA":
+			driver = WebDriverManager.operadriver().create();
+			break;
+		default:
+			throw new RuntimeException("Please check your browser name!");
+			
 		}
-		
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get("https://demo.nopcommerce.com/");
-		// Sau khi mở url thì chuyển vào trang homepage ==> Khởi tạo trạng homepage
-		
+
 		return driver;
 	}
-
-	
 
 }
